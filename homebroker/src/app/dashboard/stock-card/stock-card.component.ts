2dfabe983a465e791987c80c8aec745d83b5a0a8
Davi Araújo <Davi Arajo>
2021-06-29 23:23:48 -0300

    update: correct error

--- homebroker/src/app/dashboard/stock-card/stock-card.component.ts
+++ homebroker/src/app/dashboard/stock-card/stock-card.component.ts
@@ -18,7 +18,7 @@ export class StockCardComponent implements OnInit {
 
   get variation(): string {
     const localizedVariationString = this.stock.variation.toLocaleString();
-    const prefix = this.stock.variation > 0 ? '+' : '-';
+    const prefix = this.stock.variation >= 0 ? '+' : '-';
     return `${prefix} ${localizedVariationString.replace('-', '')}%`;
   }
 }
