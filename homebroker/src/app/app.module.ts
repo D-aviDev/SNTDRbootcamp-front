27e283e62d9b429eeef1bff8091df45e15ed1a6f
Davi Araújo <Davi Arajo>
2021-06-29 22:57:20 -0300

    first version of Screen Quotes frontend project

---
+++ homebroker/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { SharedModule } from './shared/shared.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+
+import localPT from '@angular/common/locales/pt';
+
+registerLocaleData(localPT);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule,
+    SharedModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR'}
+  ],
+
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ homebroker/src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+class Stock {
+  id: number;
+  name: string;
+  price: number;
+  minimum: number;
+  exlusiveMinimum: boolean;
+  variation: number;
+  date: string;
+}
+
+export default Stock;
